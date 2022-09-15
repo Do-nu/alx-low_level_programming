4d995ebca63c3191dcd403cb6eba8008665909ba
Donubari Forghe <donuforghe@gmail.com>
2022-09-15 16:00:13 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 through 9
+ *
+ * followed by a new line
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
+
