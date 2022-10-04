04bcc093555000511d37eb79cfb58d5827d2e75b
Donubari Forghe <donuforghe@gmail.com>
2022-10-04 14:38:47 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,39 +13,42 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	char *deliver;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (s1 == NULL)
-		s1 = "\0";
-
-	if (s2 == NULL)
-		s2 = "\0";
-
-	i = _strlen(s1);
-	j = _strlen(s2);
-
-	deliver = malloc((i + j) * sizeof(*s1) + 1);
-	if (deliver == 0)
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	strcat(deliver, s1);
-	strcat(deliver, s2);
-	return (deliver);
-}
 
-/**
- * _strlen - gets the length of a string
- * @s: the string of which length is required
- *
- * Return: length of the string
- */
+	i = 0;
+	j = 0;
 
-int _strlen(const char *s)
-{
-	int i = 0;
+	if (s1)
+	{
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
+	}
 
-	for (; s[i] != '\0'; i++)
-		;
-	return (i);
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+
+	}
+
+	s3[i] = '\0';
+
+	return (s3);
 }
