4dbbb2e03858b017bde13dbe2c0d87886686901d
Donubari Forghe <donuforghe@gmail.com>
2022-10-11 12:36:06 -0700

    Task 4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,11 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+
 	if (d->name == NULL)
-		d-> = "(nil)";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d-> = "(nil)";
+		d->owner = "(nil)";
+	
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
