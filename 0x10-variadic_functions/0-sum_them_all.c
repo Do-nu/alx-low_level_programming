e70dbb1e22c731298158037ce7183ca4f3d7892f
Donubari Forghe <donuforghe@gmail.com>
2022-10-14 16:21:50 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,11 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(num, n);
 
-	if (n == 0)
-		return (0);
 	for (i = 0; i < n; i++)
 		sumNum += va_arg(num, int);
 
 	va_end(num);
+
 	return (sumNum);
 }
