bae23a26a8df97b02e87d7d72c67884a9c17bde4
Donubari Forghe <donuforghe@gmail.com>
2022-10-28 07:03:11 -0700

    Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int converted_decimal = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		converted_decimal = 2 * converted_decimal + (b[i] - '0');
+	}
+
+	return (converted_decimal);
+}
